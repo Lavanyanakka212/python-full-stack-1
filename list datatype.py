550d1aaad880b2cb1260321a19bb31d842ec2e6d
Lavanyanakka212 <n.lavanaya25@gmail.com>
2026-06-27 20:49:59 +0530

    Add files via upload

---
+++ list datatype.py
@@ -0,0 +1,115 @@
+                                                              Datatypes:
+                                                                 
+                         ---->list is a collection of different datatype tjat are enclosed in[].
+                         ----->separeted by(,)
+                        ------>list is muttable
+                                eg: all_type[1,'python',[1,2]]
+
+                                                methods:
+                        1.append()---> this is used to add new item into lists,but it will add in the last index position
+                                                          
+                           eg:ant_=[1,2,3,4]
+                                    print(any_)
+                                    any_.append(5)
+                                    print(any_)
+                                    any.append(10)
+                                    print(any_)
+
+                                    muttable----->                                        immuttable----->
+                                    1.the datatype  can be modified                      1. can't be modified
+                                     eg:ant_=[1,2,3,4]
+                                    print(any_)
+                                    any_.append(5)
+                                    print(any_)
+
+                                     eg:list                                           eg:so=('python','java')
+                                    
+                                                                                       eg:string
+                     2.extend()------>this a also add a item in the last index position,but it will give each value in the each index position.
+                                 -----> it will take only
+
+                                    any_=[1,2,3,4]
+                                    any_.extend('python')
+                                    print(any_)
+                                    print(len(any_))
+                                    
+
+
+
+                                    eg:any_=[1,2,'python is a language',[45,78,"java is a language",[1,23]90],'hello'}
+                                    print(any_[3])
+                                    
+                     3.pop()---------->used to del the value from the list,but it will del based on index position.
+                     syntax------->variable_name.pop(index position)
+                     
+                                     eg:any_=[1,2,45,78,23,90]
+                                         any_.pop(1)
+                                          print(any_)
+
+                     4.remove()-------> used to del the from the list but it will del direct value from list.
+                     syntax-------->variable_name.remove(value)
+                     
+                                 eg:any_=[1,2,45,78,23,90]
+                                    any_.remove(90)
+                                     print(any_)
+                           """
+
+                      index=[56,[1,2],['python','java',[1,2,3,'python is a language',153,90],[78,6],'I know C']]
+                            find 153,know
+                            index,count ,insit
+                            """
+
+
+
+                                                                   tuple:
+                                                                       
+                     ------>tuple is a collection of different datatypes represent in() and separated by ,
+                     ------>it is immutable.
+                     
+                                                 methods:
+                                                     
+                                            1.index()
+                                            2.count()
+
+                               eg:how=(1,2,3,4,"python",[4,5],(90,78))
+                               print(how)
+                               print(how[4])
+                               print(how.count(90))
+
+                                eg:any_=(90,45,23,1)
+                                print(sorted(any_))
+                                print(sort(any_))
+                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                     
