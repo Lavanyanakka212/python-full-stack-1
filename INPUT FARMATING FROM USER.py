095790dd915c06e3a49ee16a1dbd194979d70559
Lavanyanakka212 <n.lavanaya25@gmail.com>
2026-06-30 12:36:54 +0530

    Add files via upload

---
+++ INPUT FARMATING FROM USER.py
@@ -0,0 +1,91 @@
+"""INPUT FARAMATING FROM USER
+INPUT()
+------->Theinput() function is used to take from the user
+
+1.int----->
+eg:
+num=int(input("enter a number:"))
+num_2=int(input("enter a number:"))
+print(num+num_2)
+
+2. string----->
+eg:
+how=input("enter a char:")
+print(how+lavi)
+
+3.float----->
+eg:
+num=float(input("enter a salary:"))
+print(num,"is the monthly salary")
+
+4.list----->
+eg:
+group_=list(map(int,input().split()))
+print(group_)
+eg:
+group=list(input())
+print(group_)
+
+5.tuple---->
+eg:
+group_=tuple(map(int,input().split()))
+print(group_)
+group_=tuple(input().split())
+print(group_)
+
+num=eval(input("enter:")#eval means we can give any datatype
+print(type(num))
+
+
+name_=input("enter your name:")
+age_=int(input("enter your age:")
+print(name_,"your name is",age_)#separated by (,)
+
+
+
+name_=input("enter your name:")
+age_=int(input("enter your age:"))
+print(name_,"your name is",age_)
+print(f"{name_} your nameis {age_}")"""#f"&doc function are same
+
+name_=input("enter your name:")
+age_=int(input("enter your age:"))
+print("my self is %s and %d years old" %(name_,age_))#modules format
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
